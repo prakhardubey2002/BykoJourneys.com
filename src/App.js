bb38161ccb418b7a79f83a5836164a03cafe538f
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-12-26 22:10:38 +0530

    react router added,routes added,uppertab added,css intialised

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import { useGlobalContext } from './context/AppContext';
+import UpperTab from './Components/UpperTab/UpperTab';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home'
+import Blog from './Pages/Blog'
 function App() {
-  const {destinationarray}=useGlobalContext();
+  const { destinationarray } = useGlobalContext();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-         
-        </p>
-        {destinationarray.map(arr=>(
-          <p>
-            {arr}
-          </p>
-        ))}
-       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UpperTab />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blog' element={<Blog/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/Components/UpperTab/UpperTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./style.module.css"
+const UpperTab = () => {
+  return (
+   <>
+   UpperTab
+   </>
+  )
+}
+
+export default UpperTab
