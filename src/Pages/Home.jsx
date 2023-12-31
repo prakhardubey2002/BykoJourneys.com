8a0e46bc32c2f00c571bef8b30cf23cf69019020
Prakhar Dubey <prakhardubey2002@gmail.com>
2024-01-01 01:06:36 +0530

    Home section completed,navbar css updated,Review Animation Json updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,10 +3,13 @@ import Typewriter from 'typewriter-effect';
 import SearchIcon from '@mui/icons-material/Search';
 import Lottie from "lottie-react";
 import GoogleLottie from '../animation/GoogleLottie.json'
+import FacebookLottie from '../animation/FacebookLottie.json'
+import ReviewLottie from '../animation/ReviewLottie.json'
+import StarIcon from '@mui/icons-material/Star';
 const Home = () => {
   return (
     <div className="Home">
-      <div className="head">
+      <div className="Homehead">
         <h2>Reboot the magic of travel with</h2>
         <Typewriter
           className="hometypewritereffect"
@@ -14,26 +17,30 @@ const Home = () => {
             strings: ['24*7 support', 'Hassle-free booking'],
             autoStart: true,
             loop: true,
+
           }}
         />
       </div>
       <div className="homesearch">
         <input type="text" placeholder='Search Destination' />
-        <SearchIcon/>
+        <SearchIcon fontSize='large' className='searchicon' />
       </div>
       <div className="homereview">
-          <div className="google">
-          <Lottie className='homeanime'  animationData={GoogleLottie} loop={true} />
-          <h4>50,000 Reviews</h4>
-          </div>
-          <div className="google">
-          <Lottie className='homeanime'  animationData={GoogleLottie} loop={true} />
-          <h4>50,000 Reviews</h4>
-          </div>
-          <div className="google">
-          <Lottie className='homeanime'  animationData={GoogleLottie} loop={true} />
-          <h4>50,000 Reviews</h4>
-          </div>
+        <div className="review">
+          <Lottie className='homeanime' animationData={GoogleLottie} loop={true} />
+          <h3>6000 Google review</h3>
+          <h4> <StarIcon className='star' />4.8</h4>
+        </div>
+        <div className="review">
+          <Lottie className='homeanime' animationData={FacebookLottie} loop={true} />
+          <h3>7000 Facebook Reviews</h3>
+          <h4>  <StarIcon className='star' /> 4.5</h4>
+        </div>
+        <div className="review">
+          <Lottie className='homeanime' animationData={ReviewLottie} loop={true} />
+          <h3>7000 Trip Advisor Reviews</h3>
+          <h4>  <StarIcon className='star' /> 4.7</h4>
+        </div>
       </div>
     </div>
   )
