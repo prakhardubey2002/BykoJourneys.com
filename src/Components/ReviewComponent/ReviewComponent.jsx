5b8fb15a8b5788af568e76cca8b64084be21aa0e
Prakhar Dubey <prakhardubey2002@gmail.com>
2024-01-07 01:13:38 +0530

    Review section completed and Responsive css added

--- src/Components/ReviewComponent/ReviewComponent.jsx
+++ src/Components/ReviewComponent/ReviewComponent.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import styles from "./style.module.css"
+import StarIcon from '@mui/icons-material/Star';
 const ReviewComponent = ({ data }) => {
     return (
-        <div style={styles.reviewcomponent} >
-            <div>ReviewComponent : {data.name} </div>
+        <div className={styles.reviewcomponent} >
+            <div className={styles.left} >
+                <img src={data.ImgUrl} alt={data.name} />
+                <div className={styles.topcontent} >
+                    <div className={styles.text}>
+                        <h2>{data.name}</h2>
+                        <p>{data.Date}</p>
+                    </div>
+                        <div className={styles.star} >
+                            <span> < StarIcon className={styles.staricon} /></span>
+                            <span> < StarIcon className={styles.staricon} /></span>
+                            <span> < StarIcon className={styles.staricon} /></span>
+                            <span> < StarIcon className={styles.staricon} /></span>
+                            <span> < StarIcon className={styles.staricon} /></span>
+
+                        </div>
+                    
+                </div>
+            </div>
+            {/* <br /> */}
+            <div className={styles.right} >
+
+                <div className={styles.reviewContent} >
+                    <span className={styles.textstart} >“</span>
+                    <p className={styles.reviewtext} >
+                        {data.review.substring(0, 200)}...
+                        <a href={data.mobReadmoreLink} target="_blank" rel="noopener noreferrer">Read More</a>
+                    </p>
+                    <br />
+                    <a href={data.ReadmoreLink} target="_blank" rel="noopener noreferrer">Read More</a>
+                </div>
+
+            </div>
         </div>
 
 
